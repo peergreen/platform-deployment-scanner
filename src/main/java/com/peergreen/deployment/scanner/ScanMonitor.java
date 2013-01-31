6d8eaf9d0e873ba99967a6579da85c1fe8908847
Florent Benoit <Florent.Benoit@peergreen.com>
2013-01-31 15:09:18 +0100

    Update of the scanner monitor

--- src/main/java/com/peergreen/deployment/scanner/ScanMonitor.java
+++ src/main/java/com/peergreen/deployment/scanner/ScanMonitor.java
@@ -1,14 +1,34 @@
+/**
+ * Copyright 2013 Peergreen S.A.S.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package com.peergreen.deployment.scanner;
 
 import java.io.File;
+import java.io.FileFilter;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.felix.ipojo.annotations.Component;
 import org.apache.felix.ipojo.annotations.Instantiate;
+import org.apache.felix.ipojo.annotations.Invalidate;
 import org.apache.felix.ipojo.annotations.Provides;
 import org.apache.felix.ipojo.annotations.Requires;
 import org.apache.felix.ipojo.annotations.Validate;
@@ -17,12 +37,11 @@ import com.peergreen.deployment.Artifact;
 import com.peergreen.deployment.ArtifactBuilder;
 import com.peergreen.deployment.DeploymentMode;
 import com.peergreen.deployment.DeploymentService;
-import com.peergreen.deployment.report.DeploymentStatusReport;
 
 @Component
 @Provides
-@Instantiate(name="Scan monitor")
-public class ScanMonitor {
+@Instantiate(name="Directoy scanner monitor")
+public class ScanMonitor extends Thread {
 
     @Requires
     private DeploymentService deploymentService;
@@ -30,129 +49,320 @@ public class ScanMonitor {
     @Requires
     private ArtifactBuilder artifactBuilder;
 
+    /**
+     * Scan interval before trying to detect new files in some directories.
+     */
+    private long scanInterval = 5000L;
 
+    /**
+     * Some actions needs to be done on Windows only.
+     */
+    private final static boolean WINDOWS_OS = System.getProperty("os.name").indexOf("win") >= 0;
 
-    protected Artifact getArtifact(String name)  {
-        URL url = ScanMonitor.class.getResource("/" + name);
-        try {
-            return artifactBuilder.build(name, url.toURI());
-        } catch (URISyntaxException e) {
-            throw new IllegalArgumentException("Invalid URI");
-        }
+    /**
+     * Directories that will be scanned for deploying new artifacts.
+     */
+    private List<File> monitoredDirectories;
+
+    /**
+     * Thread should be stopped.
+     */
+    private boolean stopThread = false;
+
+    /**
+     * List of Files that we've already sent to the deployer service.
+     */
+    private final List<File> trackedByDeploymentService;
+
+    /**
+     * Map between a File and its last length known.
+     */
+    private Map<File, Long> fileLengths = null;
+
+    private FileFilter fileFilter;
+
+    public ScanMonitor() {
+        this.monitoredDirectories = new LinkedList<File>();
+        this.trackedByDeploymentService = new ArrayList<File>();
+        this.fileLengths = new HashMap<File, Long>();
+        this.setName("Peergreen Directories Scanner");
     }
 
-
-    protected List<Artifact> getArtifacts() {
-        File bundlesDir  = new File(System.getProperty("user.dir"), "deployment-system");
-        if (!bundlesDir.exists()) {
-            bundlesDir = new File("/Users/benoitf/Documents/workspace/deployment/resources/bundles/");
-        }
-
-        try {
-            System.out.println("Will deploy all modules available in " + bundlesDir.getCanonicalPath());
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        List<Artifact> artifacts = new ArrayList<Artifact>();
-        process(bundlesDir, artifacts);
-        return artifacts;
-    }
-
-
-    protected void process(File directory, List<Artifact> artifacts)  {
-        File[] children = directory.listFiles();
-        if (children != null) {
-            for (File child : children) {
-                if (child.isFile() && (child.getPath().endsWith(".jar") || child.getPath().endsWith(".xml") || child.getPath().endsWith(".properties"))) {
-                    artifacts.add(artifactBuilder.build(child.getName(), child.toURI()));
-                }
-                if (child.isDirectory()) {
-                    process(child, artifacts);
-                }
-            }
-        }
-
-
-    }
 
     @Validate
     public void startComponent() {
-        Runnable runnable = new Runnable() {
+        this.stopThread = false;
+        this.start();
+    }
 
-            @Override
-            public void run() {
-                // TODO Auto-generated method stub
-                start();
+    @Invalidate
+    public void stopComponent() {
+        this.stopThread = true;
+    }
+
+    /**
+     * Start the thread of this class. <br>
+     * It will search and deploy files to deploy.<br>
+     * In development mode, it will check the changes.
+     */
+    @Override
+    public void run() {
+
+        // Add default directory
+        if (monitoredDirectories.isEmpty()) {
+            monitoredDirectories.add(new File(System.getProperty("user.dir"), "deploy"));
+        }
+
+        for (;;) {
+            if (stopThread) {
+                // Stop the thread
+                return;
             }
-        };
 
-        System.out.println("before RUN");
-        new Thread(runnable).start();
-        System.out.println("RUN done");
+            // Save file lengths in order to avoid deploying file for which copy is in process
+            saveFileLengths();
+
+            // 20% of the monitor interval is spent to wait in order to guarantee the file integrity
+            // If the file is being copied, the file size increases in the time
+            try {
+                Thread.sleep((long) (scanInterval * 0.2));
+            } catch (InterruptedException e) {
+                throw new RuntimeException("Thread fail to sleep");
+            }
+
+            // Check new archives/containers to start
+            detectNewArchives();
+
+            // Check deleted archives
+            detectDeletedFiles();
+
+            // 80% is for waiting
+            try {
+                Thread.sleep((long) (scanInterval * 0.8));
+            } catch (InterruptedException e) {
+                throw new RuntimeException("Thread fail to sleep");
+            }
+        }
+    }
+
+    /**
+     * Save file lengths in scanned directories.
+     */
+    private void saveFileLengths() {
+        // Clear Map
+        fileLengths.clear();
+
+        for (File deployDirectory : monitoredDirectories) {
+            // Get files
+            File[] files = deployDirectory.listFiles(fileFilter);
+
+            // Next directory if there are no files to scan.
+            if (files == null) {
+                continue;
+            }
+
+            for (File file : files) {
+                try {
+                    fileLengths.put(file, getFileSize(file));
+                } catch (Exception e) {
+                    // File cannot be read
+                    // or a sub file is not completed (in case of a directory)
+                }
+            }
+        }
+    }
+
+    /**
+     * The windows related method test if the file is complete (any write
+     * operations on the given file are finished).
+     * @param file tested file (regular file only)
+     * @throws IOException if the given file is not completed (only thrown
+     *                     under windows when file is not completed)
+     */
+    private static void checkFileCompleteness(final File file) throws IOException {
+        if (file.isFile()) {
+            // Try to open an input stream on the file
+            // This should throw an exception if the file copy is not finished (Windows only)
+            try (FileInputStream fis = new FileInputStream(file)) {
+
+            }
+        }
+    }
+
+    /**
+     * Return the file or directory size.
+     * @param file a file or directory
+     * @return the total file size (files in folders included)
+     * @throws Exception if one of the analyzed files was not completed
+     */
+    private static long getFileSize(final File file) throws Exception {
+        if (file.isFile()) {
+            if (WINDOWS_OS) {
+                checkFileCompleteness(file);
+            }
+            return file.length();
+        }
+
+        // Directory case
+        long size = 0;
+        File[] childs = file.listFiles();
+        for (File child : childs) {
+            // Add the size of the child
+            size += getFileSize(child);
+        }
+
+        return size;
 
     }
 
+    //TODO: maybe that can be done by deployment system that track deleted files too
+    protected void detectDeletedFiles() {
+        for (File file : trackedByDeploymentService) {
+            if (!file.exists()) {
+                trackedByDeploymentService.remove(file);
+            }
+        }
+    }
 
-    public void start() {
+    /**
+     * Scan all files present in the monitored directories and deploy them. (if not
+     * yet tracked).
+     */
+    protected void detectNewArchives() {
+
+        for (File deployDirectory : monitoredDirectories) {
+            // get files
+            File[] files = deployDirectory.listFiles(fileFilter);
+
+            // next directory if there are no files to scan.
+            if (files == null) {
+                continue;
+            }
+
+            // Sort the files by names
+            Arrays.sort(files, new AlphabeticalOrder());
+
+            List<File> filesToDeploy = new ArrayList<File>();
+
+            // analyze each file to detect new modules that are not yet deployed.
+            for (File file : files) {
+
+                // Already tracked ?
+                if (trackedByDeploymentService.contains(file)) {
+                    // yes, then check other files
+                    continue;
+                }
+
+                // File length has changed: maybe a file copy that is not yet completed.
+                if (fileLengthHasChanged(file)) {
+                    continue;
+                }
+
+                // Add the file to files that needs to be deployed
+                filesToDeploy.add(file);
+            }
+
+            if (stopThread) {
+                // Don't deploy new archives after the reception of a stop order
+                return;
+            }
+
+
+            // Build a list of artifacts that we will send to the service
+            List<Artifact> artifacts = new ArrayList<Artifact>();
+            for (File f : filesToDeploy) {
+                // Build artifact
+                artifacts.add(artifactBuilder.build(f.getName(),f.toURI()));
+            }
+
+            //Now deploy files
+            deploy(artifacts);
+
+        }
+    }
+
+
+    /**
+     *
+     */
+    protected void deploy(List<Artifact> artifacts) {
+        if (!artifacts.isEmpty()) {
+            deploymentService.process(artifacts, DeploymentMode.DEPLOY);
+
+            // this is now tracked by the deployment service
+            for (Artifact artifact : artifacts) {
+                File f = new File(artifact.uri().getPath());
+                this.trackedByDeploymentService.add(f);
+            }
+
+        }
+    }
+
+    /**
+     * Check if the file length has changed.
+     * @param file The given file
+     * @return True if the file length has changed
+     */
+    private boolean fileLengthHasChanged(final File file) {
+        // File length not known
+        if (!fileLengths.containsKey(file)) {
+            return true;
+        }
+
+        long storedFileLength = fileLengths.get(file);
+        long currentFileLength = 0;
         try {
-            Thread.sleep(1000L);
-        } catch (InterruptedException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
+            currentFileLength = getFileSize(file);
+        } catch (Exception e) {
+            // File cannot be checked, probably the file is being written.
+            return true;
         }
-        System.out.println("Launching deployment...");
-
-
-        // artifacts
-        List<Artifact> artifacts = new ArrayList<Artifact>();
-
-        // build artifact
-       // artifacts.add(getArtifact("d.txt"));
-        //artifacts.add(getArtifact("plan1.xml"));
-
-      // artifacts.add(getArtifact("bundle1.jar"));
-        //artifacts.add(getArtifact("bundle2.jar"));
-
-
-        //artifacts.add(getArtifact("plan2.xml"));
-        //artifacts.add(getArtifact("subplan.xml"));
-
-        artifacts = getArtifacts();
-
-        // deploy
-        DeploymentStatusReport deploymentStatusReport = deploymentService.process(artifacts, DeploymentMode.DEPLOY);
-        System.out.println("Report will be printed in 5 second");
-        try {
-            Thread.sleep(5000L);
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+        if (storedFileLength != currentFileLength) {
+            // File length has changed
+            return true;
         }
-        System.out.println("Report : " + deploymentStatusReport);
 
+        // No change in file length
+        return false;
+    }
 
+    /**
+     * @return the list of directories that are tracked.
+     */
+    public List<File> getMonitoredDirectories() {
+        return monitoredDirectories;
+    }
 
-        // Sleep
-        try {
-            Thread.sleep(10000L);
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        //System.out.println("Undeploying....");
+    /**
+     * Sets the list of directories that are tracked.
+     * @param directories list of directories that are tracked.
+     */
+    public void setMonitoredDirectories(final List<File> monitoredDirectories) {
+        this.monitoredDirectories = monitoredDirectories;
+    }
 
-        //undeploy
-        //deploymentService.process(artifacts, DeploymentMode.UNDEPLOY);
+    /**
+     * Add a directory to the list of directories that are tracked.
+     * @param directory the directory to add
+     */
+    public void addMonitoredDirectory(final File monitoredDirectory) {
+        monitoredDirectories.add(monitoredDirectory);
+    }
 
+    /**
+     * Remove a directory to the list of directories to monitor.
+     * @param directory the directory to remove
+     */
+    public void removeDirectory(final File monitoredDirectory) {
+        monitoredDirectories.remove(monitoredDirectory);
+    }
 
-        // Sleep
-        /*try {
-            Thread.sleep(2000L);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        */
+    /**
+     * Set the scan interval between each directory scan.
+     * @param scanInterval value to set
+     */
+    public void setScanInterval(final int scanInterval) {
+        this.scanInterval = scanInterval;
     }
 
 }
